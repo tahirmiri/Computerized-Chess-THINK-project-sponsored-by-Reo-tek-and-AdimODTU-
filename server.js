6d5e90ac412cb59c90c623a6c09e468007e4e59b
root <root@KEENTECH-RM.localdomain>
2018-11-17 13:20:09 +0100

    8

--- server.js
+++ server.js
@@ -3,12 +3,36 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
+var cp=""
 
 app.use(express.static('public'));
 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/api', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/setpice1', function (req, res) {
+  cp="1"
+  res.end(cp)
+});
+
+app.get('/setpice2', function (req, res) {
+  cp="2"
+  res.end(cp)
+});
+
+app.get('/board', function (req, res) {
+  res.end(cp)
+});
+
+app.get('/clear', function (req, res) {
+  cp=""
+  res.end(cp)
 });
 
 app.listen(port, function () {
