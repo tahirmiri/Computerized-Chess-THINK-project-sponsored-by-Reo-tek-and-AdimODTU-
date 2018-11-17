243db5e5091b4e96f8b5e9a5b24918b7efe0207b
root <root@KEENTECH-RM.localdomain>
2018-11-17 03:48:12 +0100

    My first commit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static('public'));
 
 
@@ -9,6 +11,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000!');
 });
